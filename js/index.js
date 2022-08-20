7cb3cb961e9de4900eb2237c7a1737fe796bb1d4
morningcoffeex <facu@DESKTOP-1R0DM7B.localdomain>
2022-08-20 13:59:53 -0300

    Agregando login y redireccion a home del eCommerce

--- js/index.js
+++ js/index.js
@@ -1,14 +1,28 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
-        window.location = "products.html"
-    });
-    document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
-        window.location = "products.html"
-    });
-    document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
-        window.location = "products.html"
-    });
-});
+const INPUT = document.getElementsByClassName('form-control')
+const PASSWORD = document.querySelector('#floatingPassword')
+// const CHECKBOX = document.querySelector('#rememberMe') I'll check how this works later
+
+    document.addEventListener('DOMContentLoaded', () => {
+    const LOGINBTN = document.querySelector('#loginBtn')
+
+        LOGINBTN.addEventListener('click', (event) => {
+        event.preventDefault();
+        if (Array.from(INPUT).some ( input => input.value == '') || PASSWORD.value.length < 6)  {   
+            showAlertError();  
+        } else {
+            // showAlertSuccess() 
+            window.location.href = "home.html";
+        }
+
+        })
+    })
+//     function showAlertSuccess() {
+//     document.getElementById("alert-success").classList.add("show");
+// } 
+// not needed
+
+function showAlertError() {
+    document.getElementById("alert-danger").classList.add("show");
+}
+
+
